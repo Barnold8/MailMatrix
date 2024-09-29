8b57dc60ae3c7aa8d2b9a2aaca9b743fd038aa02
Brandon Wright <bandslover@outlook.com>
2024-09-30 00:54:59 +0100

    Getters+Setters

--- CLI-app/Email.py
+++ CLI-app/Email.py
@@ -12,4 +12,36 @@ class Email:
         self.m_subject   = subject
         self.m_platform  = platform
         self.m_timeStamp = platform
+
+    # SETTERS
+
+    def setSender(self,sender):
+        self.m_sender = sender
+
+    def setSubject(self, subject):
+        self.m_subject = subject
+
+    def setPlatform(self, platform):
+        self.m_platform = platform 
+
+    def setTimeStamp(self, timeStamp):
+        self.m_timeStamp = timeStamp
+
+    # SETTERS
+
+    # GETTERS
+
+    def getSender(self) -> any:
+        return self.m_sender
+
+    def getSubject(self) -> any:
+        return self.m_subject
+
+    def getPlatform(self) -> any:
+        return self.m_platform
+
+    def getTimeStamp(self) -> any:
+        return self.m_timeStamp
+
+    # GETTERS
         
