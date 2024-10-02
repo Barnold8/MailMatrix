eae810cb971e2f17b5dabdeb80a56534163f17a6
Brandon Wright <bandslover@outlook.com>
2024-10-02 22:24:35 +0100

    finish checking file, was supposed to be done in install branch but lack of sleep led to different ideas

--- CLI-app/check.py
+++ CLI-app/check.py
@@ -3,6 +3,16 @@ def install():
     os.system("pip install -r requirements.txt")
 
 def check():
+
+    """
+        @author: Brandon Wright - Barnold8
+
+        This function does a simple file check to see if the program has been ran for the first time or not. If it has, it will do nothing and exit cleanly.
+        Otherwise, it will run the command to install the requirements given "requirements.txt", this is so the dependencies can be installed at the first run of the program.
+
+        I did all of this so the filesize of the software in total wasnt something massive. 
+    """
+
     try:
         with open("startup.txt","r+") as file:
             contents = file.readlines()
