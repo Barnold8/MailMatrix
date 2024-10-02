14a11b31ac26178f05d13a2c351b8fc2b326be9a
Brandon Wright <bandslover@outlook.com>
2024-10-02 22:12:14 +0100

    Add a proper way to check if the program has been run yet (this is to install dependancies)

--- CLI-app/check.py
+++ CLI-app/check.py
@@ -4,13 +4,13 @@ def install():
 
 def check():
     try:
-        with open("startup.txt","w+") as file:
+        with open("startup.txt","r+") as file:
             contents = file.readlines()
             if len(contents) == 0 or int(contents[0]) != 1:
                 install()
                 file.write("1")
     except FileNotFoundError as error:
-        with open("start.bool","w") as file:
+        with open("startup.txt","w") as file:
             install()
             file.write("1")     
 
