4b4bcd9558e94bcd509c377dbff09398003c0787
Brandon Wright <bandslover@outlook.com>
2024-10-04 00:18:32 +0100

    Add a way to cache login tokens, albeit a bit bloody buggy

--- CLI-app/Outlook/Outlook.py
+++ CLI-app/Outlook/Outlook.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import configparser
 from msgraph.generated.models.o_data_errors.o_data_error import ODataError
@@ -6,41 +5,60 @@ from configparser import SectionProxy
 from azure.identity import DeviceCodeCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.users.item.user_item_request_builder import UserItemRequestBuilder
-from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import (
-    MessagesRequestBuilder)
-from msgraph.generated.users.item.send_mail.send_mail_post_request_body import (
-    SendMailPostRequestBody)
+from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import MessagesRequestBuilder
+from msgraph.generated.users.item.send_mail.send_mail_post_request_body import SendMailPostRequestBody
 from msgraph.generated.models.message import Message
 from msgraph.generated.models.item_body import ItemBody
 from msgraph.generated.models.body_type import BodyType
 from msgraph.generated.models.recipient import Recipient
 from msgraph.generated.models.email_address import EmailAddress
+import time
+
+#TODO: Two time first login (seems greet user asks for it and the login beforehand also asks, if this doesnt occur then it requires you to login twice through the CLI menu)
+#TODO: Change from access token to refresh token
+#TODO: Modularise code to make it useable and accessible
+#TODO: Adhere to Email datatype, grab relevant information from email so it can be used in the rest of the software
+
 
 class Outlook:
-    settings: SectionProxy
+
+    settings:               SectionProxy
     device_code_credential: DeviceCodeCredential
-    user_client: GraphServiceClient
+    user_client:            GraphServiceClient
+    access_token:           str
+    token_expiry:           float
 
     def __init__(self, config: SectionProxy):
+
         self.settings = config
-        client_id = self.settings['clientId']
-        tenant_id = self.settings['tenantId']
+
+        client_id = self.settings['clientId'] # Application ID 
+        tenant_id = self.settings['tenantId'] # User type
+
         graph_scopes = self.settings['graphUserScopes'].split(' ')
 
-        self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
+        self.device_code_credential = DeviceCodeCredential(client_id, tenant_id=tenant_id)
         self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
 
+        self.access_token = None
+        self.token_expiry = 0
+
     async def get_user_token(self):
-        graph_scopes = self.settings['graphUserScopes']
-        access_token = self.device_code_credential.get_token(graph_scopes)
-        return access_token.token
-    
+
+        # Check if the current token is still valid
+        if self.access_token is None or time.time() >= self.token_expiry:
+            graph_scopes = self.settings['graphUserScopes']
+            token_response = self.device_code_credential.get_token(graph_scopes)  # this is where the access token is actually used (passed to)
+            self.access_token = token_response.token
+            self.token_expiry = time.time() + token_response.expires_on - 60  # Refresh 1 minute before expiry
+
+       
+
     async def get_user(self):
-        # Only request specific properties using $select
+        await self.get_user_token()
         query_params = UserItemRequestBuilder.UserItemRequestBuilderGetQueryParameters(
             select=['displayName', 'mail', 'userPrincipalName']
         )
-
         request_config = UserItemRequestBuilder.UserItemRequestBuilderGetRequestConfiguration(
             query_parameters=query_params
         )
@@ -49,23 +67,24 @@ class Outlook:
         return user
 
     async def get_inbox(self):
+
+        await self.get_user_token()
+
         query_params = MessagesRequestBuilder.MessagesRequestBuilderGetQueryParameters(
-            # Only request specific properties
             select=['from', 'isRead', 'receivedDateTime', 'subject'],
-            # Get at most 25 results
             top=25,
-            # Sort by received time, newest first
             orderby=['receivedDateTime DESC']
         )
         request_config = MessagesRequestBuilder.MessagesRequestBuilderGetRequestConfiguration(
-            query_parameters= query_params
+            query_parameters=query_params
         )
 
         messages = await self.user_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
-                request_configuration=request_config)
+            request_configuration=request_config)
         return messages
 
     async def send_mail(self, subject: str, body: str, recipient: str):
+        token = await self.get_user_token()
         message = Message()
         message.subject = subject
 
@@ -86,8 +105,6 @@ class Outlook:
 
 
 async def main():
-
-    # Load settings
     config = configparser.ConfigParser()
     config.read(['Outlook/config.cfg', 'Outlook/config.dev.cfg'])
     azure_settings = config['azure']
@@ -96,13 +113,44 @@ async def main():
 
     await greet_user(graph)
 
+    choice = -1
+
+    while choice != 0:
+        print('Please choose one of the following options:')
+        print('0. Exit')
+        print('1. Display access token')
+        print('2. List my inbox')
+        print('3. Send mail')
+        print('4. Make a Graph call')
+
+        try:
+            choice = int(input())
+        except ValueError:
+            choice = -1
+
+        try:
+            if choice == 0:
+                print('Goodbye...')
+            elif choice == 1:
+                await display_access_token(graph)
+            elif choice == 2:
+                await list_inbox(graph)
+            elif choice == 3:
+                await send_mail(graph)
+            elif choice == 4:
+                await make_graph_call(graph)
+            else:
+                print('Invalid choice!\n')
+        except ODataError as odata_error:
+            print('Error:')
+            if odata_error.error:
+                print(odata_error.error.code, odata_error.error.message)
+
 
 async def greet_user(graph: Outlook):
     user = await graph.get_user()
     if user:
         print('Hello,', user.display_name)
-        # For Work/school accounts, email is in mail property
-        # Personal accounts, email is in userPrincipalName
         print('Email:', user.mail or user.user_principal_name, '\n')
 
 async def display_access_token(graph: Outlook):
@@ -112,41 +160,24 @@ async def display_access_token(graph: Outlook):
 async def list_inbox(graph: Outlook):
     message_page = await graph.get_inbox()
     if message_page and message_page.value:
-        # Output each message's details
         for message in message_page.value:
             print('Message:', message.subject)
-            if (
-                message.from_ and
-                message.from_.email_address
-            ):
+            if message.from_ and message.from_.email_address:
                 print('  From:', message.from_.email_address.name or 'NONE')
             else:
                 print('  From: NONE')
             print('  Status:', 'Read' if message.is_read else 'Unread')
             print('  Received:', message.received_date_time)
 
-        # If @odata.nextLink is present
         more_available = message_page.odata_next_link is not None
         print('\nMore messages available?', more_available, '\n')
 
 async def send_mail(graph: Outlook):
-    # Send mail to the signed-in user
-    # Get the user for their email address
     user = await graph.get_user()
     if user:
         user_email = user.mail or user.user_principal_name
-
         await graph.send_mail('Testing Microsoft Graph', 'Hello world!', user_email or '')
         print('Mail sent.\n')
 
 
 asyncio.run(main())
-
-
-# class Outlook:
-
-#     def __init__(self) -> None:
-#         pass
-
-
-# This is a mess
