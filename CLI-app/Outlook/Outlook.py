f508d276014884a4f39fdab79f7a31ae6cd9f9b4
Brandon Wright <bandslover@outlook.com>
2024-10-03 22:02:29 +0100

    Remove function thatll never be used

--- CLI-app/Outlook/Outlook.py
+++ CLI-app/Outlook/Outlook.py
@@ -96,39 +96,6 @@ async def main():
 
     await greet_user(graph)
 
-    choice = -1
-
-    while choice != 0:
-        print('Please choose one of the following options:')
-        print('0. Exit')
-        print('1. Display access token')
-        print('2. List my inbox')
-        print('3. Send mail')
-        print('4. Make a Graph call')
-
-        try:
-            choice = int(input())
-        except ValueError:
-            choice = -1
-
-        try:
-            if choice == 0:
-                print('Goodbye...')
-            elif choice == 1:
-                await display_access_token(graph)
-            elif choice == 2:
-                await list_inbox(graph)
-            elif choice == 3:
-                await send_mail(graph)
-            elif choice == 4:
-                await make_graph_call(graph)
-            else:
-                print('Invalid choice!\n')
-        except ODataError as odata_error:
-            print('Error:')
-            if odata_error.error:
-                print(odata_error.error.code, odata_error.error.message)
-
 
 async def greet_user(graph: Outlook):
     user = await graph.get_user()
@@ -172,9 +139,6 @@ async def send_mail(graph: Outlook):
         await graph.send_mail('Testing Microsoft Graph', 'Hello world!', user_email or '')
         print('Mail sent.\n')
 
-async def make_graph_call(graph: Outlook):
-    # TODO
-    return
 
 asyncio.run(main())
 
