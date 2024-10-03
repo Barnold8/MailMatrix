76f24ef5f8a02ee32290acc02391a6aadd2a6050
Brandon Wright <bandslover@outlook.com>
2024-10-03 16:03:09 +0100

    Remove graph class and replace it in the Outlook.py file, make sure all handling is done here for outlook client eventually

--- CLI-app/Outlook/Outlook.py
+++ CLI-app/Outlook/Outlook.py
@@ -3,9 +3,55 @@ import asyncio
 import configparser
 from msgraph.generated.models.o_data_errors.o_data_error import ODataError
 from Outlook.Graph import Graph
+from configparser import SectionProxy
+from azure.identity import DeviceCodeCredential
+from msgraph import GraphServiceClient
+from msgraph.generated.users.item.user_item_request_builder import UserItemRequestBuilder
+from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import (
+    MessagesRequestBuilder)
+from msgraph.generated.users.item.send_mail.send_mail_post_request_body import (
+    SendMailPostRequestBody)
+from msgraph.generated.models.message import Message
+from msgraph.generated.models.item_body import ItemBody
+from msgraph.generated.models.body_type import BodyType
+from msgraph.generated.models.recipient import Recipient
+from msgraph.generated.models.email_address import EmailAddress
+
+class Outlook:
+    settings: SectionProxy
+    device_code_credential: DeviceCodeCredential
+    user_client: GraphServiceClient
+
+    def __init__(self, config: SectionProxy):
+        self.settings = config
+        client_id = self.settings['clientId']
+        tenant_id = self.settings['tenantId']
+        graph_scopes = self.settings['graphUserScopes'].split(' ')
+
+        self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
+        self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
+
+    async def get_user_token(self):
+        graph_scopes = self.settings['graphUserScopes']
+        access_token = self.device_code_credential.get_token(graph_scopes)
+        return access_token.token
+    
+    async def get_user(self):
+        # Only request specific properties using $select
+        query_params = UserItemRequestBuilder.UserItemRequestBuilderGetQueryParameters(
+            select=['displayName', 'mail', 'userPrincipalName']
+        )
+
+        request_config = UserItemRequestBuilder.UserItemRequestBuilderGetRequestConfiguration(
+            query_parameters=query_params
+        )
+
+        user = await self.user_client.me.get(request_configuration=request_config)
+        return user
+
+
 
 async def main():
-    print('Python Graph Tutorial\n')
 
     # Load settings
     config = configparser.ConfigParser()
@@ -13,7 +59,7 @@ async def main():
     print(config)
     azure_settings = config['azure']
 
-    graph: Graph = Graph(azure_settings)
+    graph: Outlook = Graph(azure_settings)
 
     await greet_user(graph)
 
